a1416a8aedc2f4b5994df7dbc6cd8323ecd2c86d
Tracey Jones <tnjones12@gmail.com>
2021-10-19 16:15:53 -0500

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      searchResults: [
+        {name: "name1", artist: "artist1", album: "album1", id:1},
+        {name: "name2", artist: "artist2", album: "album2", id:2},
+        {name: "name3", artist: "artist3", album: "album3", id:3}
+      ],
+      playlistName: 'Playlist',
+      playlistTracks: [
+        {name: "Playlist1", artist: "artistName", album: "albumName", id:1},
+        {name: "Playlist2", artist: "artistName2", album: "albumName2", id:2},
+        {name: "Playlist3", artist: "artistName3", album: "albumName3", id:3}
+      ] 
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+          </div>
+        </div>
+      </div>
+    )
+  };
+}
+
+export default App;
